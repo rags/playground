c8651a2bb7d8eec88495fee73d7caf2cf8660f0b
Raghunandan Ramakrishna Rao <rags@thoughtworks.com>
2012-08-07 03:48:56 +0530

    tried out rope
    Former-commit-id: 4dffd554c36305876e85dbcaa01ef34361d83880

--- py/fib.py
+++ py/fib.py
@@ -1,14 +1,8 @@
 def fib(n=10): 
- f = [0,1]
+ fibs = [0,1]
  if n<=2:
-  return f[:n]
+  return fibs[:n]
  for i in range(2, n):
   print i  
-  f[len(f):]=[f[i-2]+f[i-1]]
- return f
-
- 
-
-    
-    
-
+  fibs[len(fibs):]=[fibs[i-2]+fibs[i-1]]
+ return fibs
