7f60b1ee53a398e724ab8e49af26b363f38ac352
Raghunandan Ramakrishna Rao <raghunr@amazon.com>
2015-05-25 02:01:29 +0530

    0 based fenwick
    Former-commit-id: f1bbb2e71ea1d8523bf2974ff6bb4f5c51674886

--- py/ds/fenwick.py
+++ py/ds/fenwick.py
@@ -1,33 +1,26 @@
-def next(index):
-    return index + lastBit(index)
-
-def prev(index):
-    return index - lastBit(index)
-
-def lastBit(num):
-    return num & -num 
         
 # A tree with 1 based index
-class Fenwick:
+class Fenwick1:
 
     def __init__(self, max, default=0):
         self.max = max
         self.tree = [default] * (max + 1)
-
-    def create(arr, default=0):
-        tree = Fenwick(len(arr), default)
+        
+    @classmethod
+    def create(cls, arr, default=0):
+        tree = Fenwick1(len(arr), default)
         for i, val in enumerate(arr):
             tree.add(i + 1, val)
         return tree
             
     def add(self, index, value):
-        assert self.isInRange(index), "index should between 1 and %d" % self.max
+        assert self.is_in_range(index), "index should between 1 and %d" % self.max
         self.tree[index] = (self.tree[index] + value) if self.tree[index] else value
-        next_ = next(index)
-        if self.isInRange(next_):
+        next_ = Fenwick1.next(index)
+        if self.is_in_range(next_):
             self.add(next_, value)
                 
-    def isInRange(self, index):
+    def is_in_range(self, index):
         return index > 0 and index <= self.max
         
     def __str__(self):
@@ -40,32 +33,112 @@ class Fenwick:
         self.add(index, value - (self[index] - self[index - 1]))
         
     def __getitem__(self, index):
-        assert index == 0 or self.isInRange(index), "index should between 0 and %d" % self.max
-        prev_ = prev(index)
-        # print(index, prev_, self.isInRange(prev_), self.tree[index])
-        return self.tree[index] if not self.isInRange(prev_) else (self[prev_] + self.tree[index])
-            
+        assert index == 0 or self.is_in_range(index), "index should between 0 and %d" % self.max
+        prev_ = Fenwick1.prev(index)
+        # print(index, prev_, self.is_in_range(prev_), self.tree[index])
+        return self.tree[index] if not self.is_in_range(prev_) else (self[prev_] + self.tree[index])
 
+    @staticmethod
+    def next(index):
+        return index + Fenwick1.lastBit(index)
+        
+    @staticmethod
+    def prev(index):
+        return index - Fenwick1.lastBit(index)
+        
+    @staticmethod
+    def lastBit(num):
+        return num & -num 
+    
+# A tree with 1 based index
+class Fenwick:
+
+    def __init__(self, max, default=0):
+        self.max = max
+        self.tree = [default] * max 
+        self.default = default
+
+    @classmethod
+    def create(cls, arr, default=0):
+        tree = Fenwick(len(arr), default)
+        for i, val in enumerate(arr):
+            tree.add(i, val)
+        return tree
+            
+    def add(self, index, value):
+        assert self.is_in_range(index), "index should be less than %d" % self.max
+        self.tree[index] = (self.tree[index] + value) if self.tree[index] else value
+        next_ = Fenwick.next(index)
+        if self.is_in_range(next_):
+            self.add(next_, value)
+                
+    def is_in_range(self, index):
+        return index > -1 and index < self.max
+        
+    def __str__(self):
+        return str(self.tree)
+
+    def __repr__(self):
+        return str(self)
+            
+    def __setitem__(self, index, value):
+        self.add(index, value - (self[index] - self[index - 1]))
+        
+    def __getitem__(self, index):
+        assert self.is_in_range(index), "index should less than %d" % self.max
+        return self.get_item(index + 1)
+        
+    def get_item(self, n):
+        sum = self.default
+        while n > 0:
+            sum += self.tree[n-1]
+            n=Fenwick.prev(n)
+        return sum
+
+    @staticmethod
+    def next(index):
+        return index | index+1
+        
+    @staticmethod
+    def prev(index):
+        return index & index-1
+    
+        
 ############################## TESTS ##############################
 
-def should_update_tree():
-    tree = Fenwick.create(list(range(1, 11)))
-    tree.add(5, 5)
-    assert tree[4] == 10
-    assert tree[5] == 20
-    assert tree[10] == 60
-    tree[5] = 5
-    assert tree[5] == 15
-    assert tree[10] == 55
-    
-def should_create_tree():
-    tree = Fenwick.create(list(range(1, 11)))
-    assert tree[10] == 55
-    assert tree[4] == 10
-    assert tree[5] == 15
+import pytest
+@pytest.mark.parametrize("BITreeImpl,i", [
+    (Fenwick1,1),
+    (Fenwick,0)
+])	
+def should_update_tree(BITreeImpl, i):
+    tree = BITreeImpl.create(list(range(1, 11)))
+    tree.add(4+i, 5)
+    assert tree[3+i] == 10
+    assert tree[4+i] == 20
+    assert tree[9+i] == 60
+    tree[4+i] = 5
+    assert tree[4+i] == 15
+    assert tree[9+i] == 55
 
-def should_create_tree_from_scratch():
-    tree = Fenwick(3)
-    tree.add(1, 2)
-    assert tree[1] == tree[2] == 2
-    assert tree[3] == 2
+@pytest.mark.parametrize("BITreeImpl,i", [
+    (Fenwick1,1),
+    (Fenwick,0)
+
+])
+def should_create_tree(BITreeImpl,i):
+    tree = BITreeImpl.create(list(range(1, 11)))
+    assert tree[9+i] == 55
+    assert tree[3+i] == 10
+    assert tree[4+i] == 15
+
+@pytest.mark.parametrize("BITreeImpl,i", [
+    (Fenwick1,1),
+    (Fenwick,0)
+
+])
+def should_create_tree_from_scratch(BITreeImpl,i):
+    tree = BITreeImpl(3)
+    tree.add(0+i, 2)
+    assert tree[0+i] == tree[i+i] == 2
+    assert tree[2+i] == 2
