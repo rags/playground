bf988e8684df20d286c4686ba81c750eac7ead82
Raghunandan Ramakrishna Rao <raghunr@amazon.com>
2015-07-14 03:17:42 +0530

    palindrome dp
    Former-commit-id: 362d9f481525f1a6bcc5b088570b9e4bfa5a66a5

--- py/dp/make_palindrome.py
+++ py/dp/make_palindrome.py
@@ -16,27 +16,58 @@ def _make_palindrome_recur(str, i, j):
 '''
 DP loop
 | 00 | 01 | 02 | 03 | 04 |
-|    | 11 | 12 | 12 | 14 |
-|    |    | 22 | 23 | 24 |
-|    |    |    | 33 | 34 |
-|    |    |    |    | 44 |
-|    |    |    |    |    |
-
-
+| -- | 11 | 12 | 12 | 14 |
+| -- | -- | 22 | 23 | 24 |
+| -- | -- | -- | 33 | 34 |
+| -- | -- | -- | -- | 44 |
 '''
+
+def make_palindrome(str):
+    n = len(str)
+    if n == 1:
+        return 0
+    dp_tbl = [[None for i in range(n)] for j in range(n)]
+    for k in range(n):
+        for i in range(n - k):
+            j = i + k
+            if k == 0:
+                dp_tbl[i][j] = str[i], 0
+                continue
+            if k < 2: #for 1st 2 loops
+                if str[i] == str[j]:
+                    dp_tbl[i][j] = str[i] + str[j], 0
+                else: # make 'xy'-> 'xyx'- a palindrome
+                    dp_tbl[i][j] = str[i] + str[j] + str[i], 1
+                continue
+            if str[i] == str[j]:
+                sub_palindrome, insertions = dp_tbl[i + 1][j - 1]
+                dp_tbl[i][j] = str[i] + sub_palindrome + str[i], insertions
+                continue
+            (sub_palindrome1, insertions1) = dp_tbl[i + 1][j]
+            (sub_palindrome2, insertions2) = dp_tbl[i][j - 1]
+            if(insertions1 <= insertions2):
+                dp_tbl[i][j] = str[i] + sub_palindrome1 + str[i], insertions1 + 1
+            else:
+                dp_tbl[i][j] = str[j] + sub_palindrome2 + str[j], insertions2 + 1
+
+    palindrome, insertions = dp_tbl[0][n - 1]
+    print("palindrome for '%s' is '%s' with %d insertions" % (str, palindrome, insertions))
+    return insertions
+                
+            
 ################################### TESTS ###################################
 
 def should_return_no_of_ops_to_make_palindrome():
-    assert make_palindrome_recur("a") == 0
-    assert make_palindrome_recur("aa") == 0
-    assert make_palindrome_recur("aba") == 0
-    assert make_palindrome_recur("abba") == 0
-    assert make_palindrome_recur("abb") == 1
-    assert make_palindrome_recur("ab") == 1
-    assert make_palindrome_recur("abcd") == 3
-    assert make_palindrome_recur("abcde") == 4
-    assert make_palindrome_recur("abbde") == 3
-    assert make_palindrome_recur("abbde") == 3
-    assert make_palindrome_recur("evilolive") == 0
-    assert make_palindrome_recur("evilolive") == 0
-    assert make_palindrome_recur("everoddoeven") == 2 #never odd or even. 2 chars missing
+    assert make_palindrome("a") == 0
+    assert make_palindrome("aa") == 0
+    assert make_palindrome("aba") == 0
+    assert make_palindrome("abba") == 0
+    assert make_palindrome("abb") == 1
+    assert make_palindrome("ab") == 1
+    assert make_palindrome("abcd") == 3
+    assert make_palindrome("abcde") == 4
+    assert make_palindrome("abbde") == 3
+    assert make_palindrome("abbde") == 3
+    assert make_palindrome("evilolive") == 0
+    assert make_palindrome("eil-liv") == 2
+    assert make_palindrome("everoddoeven") == 2 #never odd or even. 2 chars missing
